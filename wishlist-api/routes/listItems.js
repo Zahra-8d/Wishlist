fe40d6042f837694af9e94a25d5dae36e7339e5e
Zahra-8d <me@zahraweb.uk>
2021-07-20 15:04:55 +0100

    Image previews

--- wishlist-api/routes/listItems.js
+++ wishlist-api/routes/listItems.js
@@ -1,6 +1,9 @@
+const { response } = require('express');
+
 const express = require('express'),
     router = express.Router(),
-    db = require('../connection.js');
+    db = require('../connection.js'),
+    getLinkPreview = require('link-preview-js').getLinkPreview;
 
 // get all list items
 router.get('/list_all', function(req, res) {
@@ -20,11 +23,32 @@ router.get('/list_all', function(req, res) {
     let sql = `SELECT * FROM list_items WHERE list_id = `+ req.body.list_id;
     db.query(sql, function(err, data, fields) {
       if (err) throw err;
-      res.json({
-        status: 200,
-        data,
-        message: "List name retrieved successfully"
-      })
+      data.forEach((item, i) => {
+        getLinkPreview(item['url'], {
+          imagesPropertyType: "og", // fetches only open-graph images
+          // headers: {
+          //   "user-agent": "Firefox/64.0" // fetches with googlebot crawler user agent
+          // }
+        })
+        .then((preview_data) => {
+          console.log(preview_data)
+          data[i].url_title = preview_data.title;
+          data[i].url_site = preview_data.siteName;
+          data[i].url_description = preview_data.description;
+          data[i].url_image = preview_data.images;
+
+          if (i == data.length - 1) {
+            res.json({
+              status: 200,
+              data,
+              message: "List items retrieved successfully"
+            })
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
+      });
     })
 });
 
@@ -42,11 +66,11 @@ router.post('/update', function(req, res) {
 
 // create new list item
 router.post('/new', function(req, res) {
-    let sql = `INSERT INTO list_items(list_id, name, description, url) VALUES (?)`;
+    let sql = `INSERT INTO list_items(list_id, name, price, url) VALUES (?)`;
     let values = [
         req.body.id,
         req.body.name,
-        req.body.description,
+        req.body.price,
         req.body.url,
     ];
     db.query(sql, [values], function(err, data, fields) {
